a2c6b2e8e7b7df162ae8d36319bc1e3e66dee20d
Patricia Carrasco <patri.carrasco@gmail.com>
2024-03-15 15:32:22 +0100

    done get image

--- get-images-api-starter/backend/src/lambda/http/getImages.js
+++ get-images-api-starter/backend/src/lambda/http/getImages.js
@@ -1,17 +1,18 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
-const dynamoDbDocument = DynamoDBDocument.from(new DynamoDB())
+const dynamoDbDocument = DynamoDBDocument.from(new DynamoDB());
 
-const groupsTable = process.env.GROUPS_TABLE
-const imagesTable = process.env.IMAGES_TABLE
+const groupsTable = process.env.GROUPS_TABLE;
+const imagesTable = process.env.IMAGES_TABLE;
+
+console.log(imagesTable)
 
 export async function handler(event) {
-  console.log('Caller event', event)
-  const groupId = event.pathParameters.groupId
+  console.log("Caller event", event);
+  const groupId = event.pathParameters.groupId;
 
-  // TODO: Implement this function
-  const validGroupId = await groupExists(groupId)
+  const validGroupId = await groupExists(groupId);
 
   if (!validGroupId) {
     return {
@@ -19,38 +20,38 @@ export async function handler(event) {
       headers: {
         "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(value, {
-        error:'Group does not exist'
-      })
+      body: JSON.stringify({
+        error: "Group does not exist",
+      }),
     };
   }
-  const images = await getImagesPerGroup(groupId)
+
+  const images = await getImagesPerGroup(groupId);
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      items: []
-    })
-  }
+      items: images,
+    }),
+  };
 }
 
 async function groupExists(groupId) {
   const result = await dynamoDbDocument.get({
     TableName: groupsTable,
     Key: {
-      id: groupId
-    }
-  })
+      id: groupId,
+    },
+  });
 
-  console.log('Get group: ', result)
-  return !!result.Item
+  console.log("Get group: ", result);
+  return !!result.Item;
 }
 
 async function getImagesPerGroup(groupId) {
-  // TODO: Implement this function
   const result = await dynamoDbDocument.query({
     TableName: imagesTable,
     KeyConditionExpression: "groupId = :groupId",
